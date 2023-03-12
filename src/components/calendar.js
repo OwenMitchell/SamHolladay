015e8de1581df594fd66a7d4a2ab7d6c2a17fc6e
Owen Mitchell <owenrossmitchell@gmail.com>
2023-03-12 17:47:58 -0400

    extra socials with links

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -1,11 +1,15 @@
+import React from 'react';
+
 export default function Calendar(){
   return (
     <div class="App-calendar">
-      <span>Dates</span>
+      <h3>Dates</h3>
+      <img class="calendar-bracket" src={require('../images/bracket_open_down_white_on_black.jpg')}/>
       <Event venue="Horseshoe" date="Mar 3"></Event>
       <Event venue="Horseshoe" date="Mar 3"></Event>
       <Event venue="Horseshoe" date="Mar 3"></Event>
       <Event venue="Horseshoe" date="Mar 3"></Event>
+      <img class="calendar-bracket" src={require('../images/bracket_open_up_white_on_black.jpg')} />
     </div>
   );
 }
